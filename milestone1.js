b926b26608ddbf8a67ebea7facc8543d592eb056
Ha Vy Nguyen <ha.vy.nguyen@student.ehb.be>
2024-09-28 14:58:22 +0200

    opdracht milestone 1 signature klaar

---
+++ milestone1.js
@@ -0,0 +1,25 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawAvatar();
+function drawAvatar(){
+    context.fillStyle = "#EEEEEE"
+    context.beginPath();
+    context.rect(20, 20, 300, 300);
+    context.fill();
+
+    context.fillStyle = "#d77373"
+    context.beginPath();
+    context.rect(145,50,50,100);
+    context.rect(50,100,50,50);
+    context.rect(240,100,50,50);
+    context.rect(240,200,50,50);
+    context.rect(50,200,50,50);
+    context.rect(145,200,50,50);
+    context.fill();
+
+    
+}
+
