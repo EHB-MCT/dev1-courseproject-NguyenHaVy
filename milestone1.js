db9f874ff7e06d4bbbd8eeb2f37b36796af58d29
Ha Vy Nguyen <ha.vy.nguyen@student.ehb.be>
2024-10-03 14:15:23 +0200

    verbeterde versie

--- milestone1.js
+++ milestone1.js
@@ -5,12 +5,12 @@ let context = canvas.getContext("2d");
 
 drawAvatar();
 function drawAvatar(){
-    context.fillStyle = "black"
+    context.fillStyle = "black";
     context.beginPath();
     context.rect(20, 20, 300, 300);
     context.fill();
 
-    context.fillStyle = "#d77373"
+    context.fillStyle = "#d77373";
     context.beginPath();
     context.rect(145,50,50,100);
     context.rect(50,100,50,50);
